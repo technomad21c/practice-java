8b860b5cb57da4338e7c0de7eaa35ec6a5a00e46
brandon-highmarkglobaltech <brandon@highmarkglobal.tech>
2020-04-11 22:36:37 -0700

    add palindrome checker example using IntStream.noneMatch

---
+++ 01_basic/013_palindrome/src/palindrome/PalindromeChecker.java
@@ -0,0 +1,32 @@
+//https://www.baeldung.com/java-palindrome
+package palindrome;
+
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+public class PalindromeChecker {
+	public static void main(String[] args) {
+		String text = "abcedecba";	
+		
+		if (isPalindromeUsingIntStream(text))
+			System.out.println("TEXT: " + text + " is palindrome");
+		else
+			System.out.println("TEXT: " + text + " is not palindrome");
+	}
+	
+	public static boolean isPalindromeUsingIntStream(String text) {
+//		 noneMatch() returns true if none of the stream elements match the given predicate
+		String temp = text.replaceAll("\\s+", "").toLowerCase();
+		return IntStream.range(0, temp.length() / 2).noneMatch(i -> temp.charAt(i) != temp.charAt(temp.length() - i -1));
+	}
+	
+	public boolean isPalindromeReverseTheString(String text) {
+	    StringBuilder reverse = new StringBuilder();
+	    String clean = text.replaceAll("\\s+", "").toLowerCase();
+	    char[] plain = clean.toCharArray();
+	    for (int i = plain.length - 1; i >= 0; i--) {
+	        reverse.append(plain[i]);
+	    }
+	    return (reverse.toString()).equals(clean);
+	}
+}
